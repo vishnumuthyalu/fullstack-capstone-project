6f595a55ccf748fdf5acc410e59bfe017e05ac66
Vishnu Muthyalu <vm17college@gmail.com>
2025-12-02 17:28:17 -0500

    Added search API routes and gift routes to backend

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-
+const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
@@ -37,6 +37,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
