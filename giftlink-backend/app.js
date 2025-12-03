5d998e89e6a66464ef70ccd1149345e88a2c8ef8
Vishnu Muthyalu <vm17college@gmail.com>
2025-12-03 09:36:11 -0500

    back-end integaration for registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
@@ -38,7 +39,7 @@ app.use(pinoHttp({ logger }));
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 //{{insert code here}}
 app.use('/api/gifts', giftRoutes);
-
+app.use('/api/auth', authRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
 
